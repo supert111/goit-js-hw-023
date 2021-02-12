4352dd43099e7ed644b9a88feaca0e8c37148c10
supert111 <31413087+supert111@users.noreply.github.com>
2021-02-12 16:34:10 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import './styles.css';
 import './menu.json';
+
+// const Theme = {
+//     LIGHT: 'light-theme',
+//     DARK: 'dark-theme',
+//   };
+
+//   const themeSwitchToggleRef = document.querySelector('#theme-switch-toggle');
+//   console.log(themeSwitchToggleRef);
+  
+//   const bodyRef = document.querySelector('body');
+//   console.dir(bodyRef);
+//   if (themeSwitchToggleRef) {
+//     bodyRef.classList.remove(Theme.DARK); 
+//     bodyRef.classList.add(Theme.LIGHT);
+//     console.log(Theme.LIGHT);
+//   }
+//   else {
+//     bodyRef.classList.remove(Theme.LIGHT); 
+//     bodyRef.classList.add(Theme.DARK);
+//   }
